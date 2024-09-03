374e14decb646119fe6197c21481f41ca4e8909d
Yusuf <starbucksandapple@gmail.com>
2024-09-03 20:15:45 +0300

    Identity Server entegration

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.IdentityServer.Dtos;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.Username,
+                Email = signUpDto.Email,
+                City = signUpDto.City
+            };
+
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+
+        
+    }
+}
